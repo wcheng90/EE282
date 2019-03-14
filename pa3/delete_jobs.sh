d4248d78a3c2b3fae12874bebe3505797485f0ee
Danny <dannybob@stanford.edu>
2019-03-14 05:12:37 -0700

    Fixed yaml files

---
+++ pa3/delete_jobs.sh
@@ -0,0 +1,8 @@
+kubectl delete job/parsec-dedup
+kubectl delete job/parsec-blackscholes
+kubectl delete job/parsec-ferret
+kubectl delete job/parsec-freqmine
+kubectl delete job/parsec-canneal
+kubectl delete job/parsec-splash2x-fft
+
+
