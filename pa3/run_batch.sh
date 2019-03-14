02d042659a96fd69f8a5bb3ab0f3de269cbe458f
Danny <dannybob@stanford.edu>
2019-03-14 08:43:51 -0700

    added run_bacth.sh

---
+++ pa3/run_batch.sh
@@ -0,0 +1,6 @@
+kubectl create -f parsec-dedup.yaml
+kubectl create -f parsec-blackscholes.yaml
+kubectl create -f parsec-freqmine.yaml
+kubectl create -f parsec-ferret.yaml
+kubectl create -f parsec-canneal.yaml
+kubectl create -f parsec-fft.yaml
